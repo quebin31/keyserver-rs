c136e4f1cfa5d9e2e2afca86ce1a7630d627d05e
Harry Barber <harrybarber@protonmail.com>
2020-05-31 12:24:11 +0100

    Add database tests

--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,6 @@
 use std::sync::Arc;
 
-use prost::Message as PMessage;
+use prost::Message as _;
 use rocksdb::{Error as RocksError, Options, DB};
 
 use crate::models::{database::DatabaseWrapper, keyserver::Peers};
@@ -19,6 +19,7 @@ impl Database {
         DB::open(&opts, &path).map(Arc::new).map(Database)
     }
 
+    /// Get raw `DatabaseWrapper` from the database.
     pub fn get_raw_metadata(&self, addr: &[u8]) -> Result<Option<Vec<u8>>, RocksError> {
         let key = [&[METADATA_NAMESPACE], addr].concat();
         self.0.get(key)
@@ -58,5 +59,73 @@ impl Database {
     /// Put serialized `Peers` to database.
     pub fn put_peers(&self, raw: &[u8]) -> Result<(), RocksError> {
         self.0.put([PEER_NAMESPACE], raw)
+    }
+}
+
+#[cfg(test)]
+pub mod tests {
+    use rocksdb::{DB, Options};
+
+    use crate::models::{database::DatabaseWrapper, keyserver::{Peers, Peer}};
+    use super::*;
+
+    #[test]
+    fn peers() {
+        const TEST_NAME: &str = "./tests/peer";
+
+        // Create database
+        let database = Database::try_new(TEST_NAME).unwrap();
+        
+        // Create peers
+        let peer_a = Peer {
+            url: "url a".to_string()
+        };
+        let peer_b = Peer {
+            url: "url b".to_string()
+        };
+        let peers_in = Peers {
+            peers: vec![peer_a, peer_b]
+        };
+        let mut peers_raw = Vec::with_capacity(peers_in.encoded_len());
+        peers_in.encode(&mut peers_raw).unwrap();
+
+        // Put to database
+        database.put_peers(&peers_raw).unwrap();
+
+        // Get from database
+        let peers_out = database.get_peers().unwrap().unwrap();
+        assert_eq!(peers_in, peers_out);
+
+        // Destroy database
+        drop(database);
+        DB::destroy(&Options::default(), TEST_NAME).unwrap();
+    }
+
+    #[test]
+    fn metadata() {
+        const TEST_NAME: &str = "./tests/metadata";
+
+        // Create database
+        let database = Database::try_new(TEST_NAME).unwrap();
+
+        // Create database wrapper
+        let database_wrapper_in = DatabaseWrapper {
+            token: vec![0,1,3,4],
+            serialized_auth_wrapper: vec![2,3, 4]
+        };
+        let mut database_wrapper_raw = Vec::with_capacity(database_wrapper_in.encoded_len());
+        database_wrapper_in.encode(&mut database_wrapper_raw).unwrap();
+
+        // Put to database
+        let addr = vec![0,3,4,3,2];
+        database.put_metadata(&addr, &database_wrapper_raw).unwrap();
+
+        // Get from database
+        let data_wrapper_out = database.get_metadata(&addr).unwrap().unwrap();
+        assert_eq!(database_wrapper_in, data_wrapper_out);
+
+        // Destroy database
+        drop(database);
+        DB::destroy(&Options::default(), TEST_NAME).unwrap();
     }
 }
